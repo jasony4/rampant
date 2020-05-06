fa5a5e9c5d13df74a4c6c9e2f643274514380325
jason <jasony4@gmail.com>
2020-05-06 14:37:40 +0100

    added ptsTest html and js

--- ptsTest.js
+++ ptsTest.js
@@ -1,14 +1,21 @@
+// add Pts to global scope
 Pts.namespace( window );
+var run = Pts.quickStart("#canvasTest", "#66FCF1");
+run( function(time,ftime){
+    //create rectangle from center of space with size being half the spaces size
+    // Rectangle.fromCenter(center, size) : stores a group of 2 pts (top-left, bottom-right)
+var rect = Rectangle.fromCenter(space.center, space.size.$divide(4));
+// create a polygon by taking rects corners and creating 4 pts (,,,)
+var poly = Rectangle.corners( rect );
+// use the shear 2d function to shear the polygon at the spaces center
+// amount of shearing cycles between -0.5 to 0.5 every 5 seconds
+// shape.shearfunction(cycle , shear from)
+poly.shear2D(Num.cycle(time%5000/5000)-0.5, space.center);
+//fill polygon in gold
+form.fillOnly("#fcf923").polygon(poly);
+//stroke rectangle 
+form.strokeOnly("#fff", 3).rect( rect );
 
-var run = Pts.quickStart( "#hello", "#fff", true );
 
-// quickStart returns a function wrapper for use in animation loop, eg:
-run( function(time, ftime) { 
-    space.add( (time , ftime) => {
-        var rect = Rectangle.fromCenter( space.center, space.size.$divide(2));
-        var poly = Rectangle.corners( rect );
-        poly.shear2D(Num.cycle( time%5000/5000 ) - 0.5, space.center );
-        form.fillOnly("#1f2833").polygon( poly );
-        form.strokeOnly("#45A29E ",3).rect( rect );
-    });
-} );
+});
+
