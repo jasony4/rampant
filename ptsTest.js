f98a47fe872910a2fa799ef77a351a060c8bc88b
jason <jasony4@gmail.com>
2020-05-11 14:22:35 +0100

    added sections with 100vh

--- ptsTest.js
+++ ptsTest.js
@@ -16,19 +16,21 @@ Pts.quickStart( "#canvasTest", "#1f2833" );
   var pts6 = new Group();
   var pts7 = new Group();
   var pts8 = new Group();
+  var pts9 = new Group();
  
 
   space.add( { 
 
     // init with 500 random points 
-         start:  (bound) => { pts = Create.distributeRandom( space.innerBound, 100);
-                              pts2 = Create.distributeRandom( space.innerBound, 100);
-                            pts3 = Create.distributeRandom( space.innerBound, 100);
-                          pts4 = Create.distributeRandom( space.innerBound, 100);
-                        pts5 = Create.distributeRandom( space.innerBound, 100);
+         start:  (bound) => { pts = Create.distributeRandom( space.innerBound, 200);
+                              pts2 = Create.distributeRandom( space.innerBound, 200);
+                            pts3 = Create.distributeRandom( space.innerBound, 200);
+                          pts4 = Create.distributeRandom( space.innerBound, 200);
+                        pts5 = Create.distributeRandom( space.innerBound, 200);
                         pts6 = Create.distributeRandom( space.innerBound, 200); 
                         pts7 = Create.distributeRandom( space.innerBound, 2); 
-                        pts8 = Create.distributeRandom( space.innerBound, 100); 
+                        pts8 = Create.distributeRandom( space.innerBound, 300);
+                       
 
                   },
     
@@ -38,30 +40,31 @@ Pts.quickStart( "#canvasTest", "#1f2833" );
 
       
       // check if each point is within circle's range
-      for (let i=0, len=pts6.length; i<len; i++) {
+      for (let i=0, len=pts8.length; i<len; i++) {
 
 
-          form.fillOnly("whitesmoke").point(pts[i], 1);
+          form.fillOnly("whitesmoke").point(pts[i], 0.5);
           form.fillOnly("white").point(pts2[i], 0.5);
           form.fillOnly("whitesmoke").point(pts3[i], 0.5);
           form.fillOnly("whitesmoke").point(pts4[i], 0.5);
           form.fillOnly("whitesmoke").point(pts5[i], 1.5);
           form.fillOnly("#fcf923").point(pts6[i], 0.5);
           form.fillOnly("#fcf923").point(pts7[i], 1);
-          form.fillOnly("#1f2833").point(pts8[i], 25);
+          form.fillOnly("#1f2833").point(pts8[i], 5);
+       
           
           
            
 
       }
-      pts.rotate2D( 0.0002, space.center);
+     // pts.rotate2D( 0.0002, space.center);
       pts2.rotate2D( 0.0003, space.center);
       pts3.rotate2D( 0.0004, space.center);
       pts4.rotate2D( -0.0005, space.center);
       pts5.rotate2D( -0.0004, space.center);
       pts6.rotate2D( -0.0003, space.center);
       pts7.rotate2D( -0.005, space.center);
-      pts8.rotate2D( -0.025, space.center);
+      pts8.rotate2D( -0.02, space.center);
 
       
       
